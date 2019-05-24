c35825b6be9e7e4b231fc9461b4f05b4df7eed7d
kyeongmin.kang <kyeongmin.kang@gluwa.com>
2019-05-24 18:47:40 +0900

    Board 일단 복원

---
+++ MvcBoardApp/MvcBoardApp/Migrations/20190524051535_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcBoardApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Board",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: true),
+                    Subject = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    WriteTime = table.Column<DateTime>(nullable: false),
+                    Hits = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Board", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Board");
+        }
+    }
+}
---
+++ MvcBoardApp/MvcBoardApp/Models/Board.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MvcBoardApp.Models
+{
+    public class Board
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("UserName")]
+        public string UserName { get; set; }
+
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime WriteTime { get; set; }
+
+        public int Hits { get; set; }
+
+      
+       
+        
+    }
+}
